03beddbf59c019dd3e281bdf78b466b4692432e0
Balastov <m0000nspe1111@yandex.ru>
2026-02-10 23:52:08 +0300

    Комментарий в Домашнем Задании - ссылка

--- task-tracker/models.py
+++ task-tracker/models.py
@@ -129,7 +129,7 @@ class TaskType(db.Model):
 class Homework(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    comment = db.Column(db.String(500), nullable=True)
+    comment = db.Column(db.Text, nullable=True)
 
     def to_dict(self):
         return {
